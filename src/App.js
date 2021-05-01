202c3fb2df21fe8a4b234aa33e0c5ff2fce36a63
Andrew Nicholls <apnicholls56@yahoo.com>
2021-05-01 14:55:33 -0500

    check in

--- src/App.js
+++ src/App.js
@@ -5,11 +5,15 @@ function App() {
 
   const [categories, setCategories] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState();
+  const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState();
+  const [contents, setContents] = useState([]);
+  const [selectedContent, setSelectedContent] = useState();
 
 
   const fetchCategories = async () => {
     console.log('this will fetch all the food categories');
-    let res = await fetch('http://localhost:3000/api/v1/categories');
+    let res = await fetch('${process.env.API_URL}/api/v1/categories');
     let data = await res.json();
     console.log(data);
     setCategories(data);
@@ -17,8 +21,24 @@ function App() {
 
   useEffect(() => {
     fetchCategories()
-    /* This code will run only one on pag load or component mount / load and load an empty array*/
+    /* This code will run only one on page load or component mount / load and load an empty array unless selectedCategory has been populated*/
   }, [])
+
+  const fetchRecipes = async (id) => {
+    console.log('this will fetch all recipes within a category id', id);
+    let res = await fetch(`http://localhost:3000/api/v1/categories/${id}/recipes`);
+    let data = await res.json();
+    console.log(data);
+    setRecipes(data);
+  };
+
+  const fetchContents = async (id) => {
+    console.log('this will fetch the content for a selected recipe', id);
+    let res = await fetch(`http://localhost:3000/api/v1/recipes/${id}/contents`);
+    let data = await res.json();
+    console.log(data);
+    setContents(data);
+  }
 
   return (
     <>
@@ -27,30 +47,44 @@ function App() {
         <div className={'col-span-full content-center border p-5'}>
           <h1 className={'text-center text-3xl'}>Recipe Book</h1>
           <ul className="flex items-stretch justify-center">
-              {categories.map((category, index) => {
-                return <li key={index}>{category.name}</li>
-              })}
-            </ul>
+            {categories.map((category, index) => {
+              return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
+                setSelectedCategory(category.id)
+                fetchRecipes(category.id);
+              }}>
+                {category.name}</li>
+            })}
+          </ul>
         </div>
       </div>
 
       {/* Left column for recipe names right column for body of recipe and instructions  */}
       <div className="grid grid-cols-12">
         <div className={'col-span-12 md:col-span-3 lg:col-span-2 border p-5'}>
-          <h3 className={'text-center text-3x l'}>Food Categories</h3>
-            <ul>
-              {categories.map((category, index) => {
-                return <li key={index} className={category.id == selectedCategory ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
-                  setSelectedCategory(category.id)
-                }}>
-                {category.name}
-                </li>
-              })};
-            </ul>
+          <h3 className={'text-center text-3x l'}>Recipes</h3>
+          <ul>
+            {recipes.map((recipe, index) => {
+              return <li key={index} className={recipe.id == selectedRecipe ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
+                setSelectedRecipe(recipe.id)
+                fetchContents(recipe.id);
+              }}>
+                {recipe.titleTxt}</li>
+            })}
+          </ul>
         </div>
 
         <div className={'col-span-12 md:col-span-9 lg:col-span-10 border p-5'}>
-          <h1 className={'text-center text-3xl'}>Recipes will go here finally</h1>
+          <h1 className={'text-center text-3xl'}>Individual recipe content and instructions will go here </h1>
+          <ul>
+            {contents.map((content, index) => {
+              return <li key={index} className={content.id == selectedContent ? 'border p-5 cursor-pointer bg-gray-200' : 'border p-5 cursor-pointer'} onClick={() => {
+                setSelectedContent(content.id)
+                fetchContents(content.id);
+              }}>
+                {content.descTxt} {content.ingredientsTxt} {content.methodTxt} {content.commentsTxt} </li>
+            })}
+          </ul>
+
         </div>
       </div>
     </>
@@ -60,6 +94,8 @@ function App() {
 export default App;
 
 /* useEffect(() => { */
-  /* This code will run everytime the someStateVariable changes*/
-  /* This code will run verytime var2 OR somestateVariable changes */
+/* This code will run everytime the someStateVariable changes*/
+/* This code will run verytime var2 OR somestateVariable changes */
 /* }, [someStateVariable, var2]) */
+
+
